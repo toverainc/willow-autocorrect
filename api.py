1be18eb39f9b79396908d2af354636f2a80d334f
Kristian Kielhofner <kris@tovera.com>
2023-11-11 11:09:49 -0600

    Big update - got carried away again

--- api.py
+++ api.py
@@ -6,6 +6,11 @@ import json
 import logging
 import requests
 
+# For typesense-server
+import subprocess
+import time
+import threading
+
 from datetime import datetime
 from zoneinfo import ZoneInfo
 from decouple import config
@@ -14,7 +19,7 @@ import typesense
 HA_URL = config('HA_URL', default="http://homeassistant.local:8123", cast=str)
 HA_TOKEN = config('HA_TOKEN', default=None, cast=str)
 LOG_LEVEL = config('LOG_LEVEL', default="debug", cast=str)
-TGI_URL = config('TGI_URL', default=None, cast=str)
+TGI_URL = config(f'TGI_URL', default=None, cast=str)
 
 # TGI model stuff
 MAX_NEW_TOKENS = config('MAX_NEW_TOKENS', default=40, cast=int)
@@ -26,12 +31,11 @@ SYS_PROMPT = config('SYS_PROMPT', default="", cast=str)
 
 # This doesn't seem to be getting from docker to here - FIX
 TYPESENSE_API_KEY = config('TYPESENSE_API_KEY', default='testing', cast=str)
-TYPESENSE_HOST = config('TYPESENSE_HOST', default='localhost', cast=str)
-TYPESENSE_PORT = config('TYPESENSE_PORT', default=9001, cast=int)
+TYPESENSE_HOST = config('TYPESENSE_HOST', default='127.0.0.1', cast=str)
+TYPESENSE_PORT = config('TYPESENSE_PORT', default=8108, cast=int)
 
 HA_URL = f'{HA_URL}/api/conversation/process'
 HA_TOKEN = f'Bearer {HA_TOKEN}'
-TGI_URL = f'{TGI_URL}/generate'
 
 print(f'Config vars: {HA_URL} {HA_TOKEN} {TGI_URL}')
 
@@ -41,6 +45,21 @@ logging.basicConfig(
     datefmt='%Y-%m-%d %H:%M:%S')
 
 # Typesense
+def start_typesense():
+    def run(job):
+        proc = subprocess.Popen(job)
+        proc.wait()
+        return proc
+
+    # Fix this in prod to use some kind of unique/user provided/etc key. Not that big of a deal but...
+    job = ['/usr/local/sbin/typesense-server', '--data-dir=/app/data/typesense', '--api-key=testing', '--log-dir=/dev/shm']
+
+    # server thread will remain active as long as FastAPI thread is running
+    thread = threading.Thread(name='typesense-server', target=run, args=(job,), daemon=True)
+    thread.start()
+    #log.info(f'Waiting {TYPESENSE_SLEEP} seconds for typesense...')
+    #time.sleep(TYPESENSE_SLEEP)
+
 ts_client = typesense.Client({
   'nodes': [{
     'host': TYPESENSE_HOST,
@@ -48,7 +67,7 @@ ts_client = typesense.Client({
     'protocol': 'http'
   }],
   'api_key': TYPESENSE_API_KEY,
-  'connection_timeout_seconds': 2
+  'connection_timeout_seconds': 1
 })
 
 app = FastAPI(title="WAC Proxy",
@@ -64,6 +83,12 @@ try:
 except:
     pass
 
+# Effectively disable for now in dev with uvicorn --reload
+@app.on_event("startup")
+async def startup_event():
+    #start_typesense()
+    pass
+
 ha_headers = {
     "Authorization": HA_TOKEN,
 }
@@ -72,7 +97,10 @@ llm_headers = {
     "Content-Type": "application/json",
 }
 
-def do_llm(text):
+def do_tgi(text, speech):
+    if TGI_URL == 'None':
+        return speech
+
     text = f'{SYS_PROMPT} {text}'
     log.info(f'Doing LLM request with text {text}')
     data = {
@@ -88,7 +116,7 @@ def do_llm(text):
     },
 }
     time_start = datetime.now()
-    response = requests.post(TGI_URL, headers=llm_headers, json=data)
+    response = requests.post(f'{TGI_URL}/generate', headers=llm_headers, json=data)
     time_end = datetime.now()
     infer_time = time_end - time_start
     infer_time_milliseconds = infer_time.total_seconds() * 1000
@@ -114,6 +142,20 @@ def get_date():
     dt_string = now.strftime("%A %B %d")
     return dt_string
 
+def learn_command(command):
+    results = do_wac_search(command, raw = False, json = False, distance = 5, num_results = 5)
+    if results is None:
+        log.info(f"Adding command '{command}' to WAC autolearn")
+        command_json = {
+            'command': command,
+            'rank': 1.0,
+            'source': 'autolearn',
+            }
+        # Use create to update in real time
+        ts_client.collections['commands'].documents.create(command_json)
+    else:
+        log.info(f"WAC already knows command '{command}'")
+
 def parse_ha(response):
     log.debug(str(response))
     speech = json_get(response, "/response/speech/plain/speech", str)
@@ -121,6 +163,7 @@ def parse_ha(response):
     code = json_get_default(response, "/response/data/code", "intent_match")
 
     if code == "no_intent_match":
+        speech = "No Willow auto correct match"
         success = False
     else:
         success = True
@@ -134,32 +177,44 @@ def do_ha(text, language):
     response = response.json()
     success, speech = parse_ha(response)
     if success:
+        log.info(f"HA Returned intent match on command '{text}' - learning")
+        learn_command(text)
         response = speech
     else:
-        response = do_llm(text)
+        response = do_tgi(text, speech)
 
     return response
 
-def wac_search(command, distance = 5, num_results = 5):
+def wac_search(command, distance = 2, num_results = 5):
     search_parameters = {
   'q'         : command,
   'query_by'  : 'command',
-  'sort_by'   : '_text_match:desc',
+  'sort_by'   : '_text_match:desc,rank:desc',
+  'text_match_type': 'max_score',
+  'prioritize_token_position': False,
+  'drop_tokens_threshold': 1,
+  'typo_tokens_threshold': 2,
+  'split_join_tokens': 'fallback',
   'num_typos' : distance,
   'per_page' : num_results
 }
     return ts_client.collections['commands'].documents.search(search_parameters)
 
-def get_first_command(command, results, search_time, json = False):
+def get_first_command(command, results, distance, search_time, json = False):
+    first_result = None
+    first_result_source = None
     try:
+        text_score = json_get(results, "/hits[0]/text_match")
+        tokens_matched = json_get(results, "/hits[0]/text_match_info/tokens_matched")
+        if tokens_matched <= 1:
+            return
         first_result = json_get(results, "/hits[0]/document/command")
         first_result_source = json_get(results, "/hits[0]/document/source")
     except:
-        first_result = None
-        first_result_source = None
-    
+        pass
+
     if json:
-        return {'input': command, 'command': first_result, 'source': first_result_source, 'search_time': search_time}
+        return {'input': command, 'command': first_result, 'source': first_result_source, 'distance': distance, 'tokens_matched': tokens_matched, 'score': text_score, 'search_time': search_time}
     else:
         return first_result
 
@@ -176,7 +231,7 @@ def do_wac_search(command, raw = False, json = False, distance = 5, num_results 
     if raw:
         return results
     else:
-        results = get_first_command(command, results, search_time_milliseconds, json)
+        results = get_first_command(command, results, distance, search_time_milliseconds, json)
         log.info(f'WAC matched: {results}')
         return results
 
@@ -193,7 +248,7 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/api/search", summary="WAC Search", response_description="WAC Search Results")
-async def api_search(request: Request, command: str, raw: Optional[bool] = False, json: Optional[bool] = True, distance: Optional[str] = 5, num_results: Optional[str] = 5):
+async def api_search(request: Request, command: str, raw: Optional[bool] = False, json: Optional[bool] = True, distance: Optional[str] = 2, num_results: Optional[str] = 5):
     log.info(f"Doing WAC search for command '{command}'")
     results = do_wac_search(command, raw, json, distance, num_results)
     if json:
